dca72d8fd9a268aa0c234beb92f3bc6be4bf3af7
harshalchalke31 <harshal.chalke2001@gmail.com>
2025-01-21 09:29:50 -0500

    config, component and entity updated

---
+++ src/textSummarizer/components/data_validation.py
@@ -0,0 +1,27 @@
+import os
+from textSummarizer.config.configuration import DataValidationConfig
+from textSummarizer.logging import logger
+
+class DataValidation:
+    def __init__(self,config: DataValidationConfig):
+        self.config = config
+    
+    def validate_all_files(self)->bool:
+        try:
+            validation_status=None
+            all_files=os.listdir(os.path.join('artifacts','data_ingestion','samsum_dataset'))
+
+            for file in all_files:
+                if file not in self.config.ALL_REQUIRED_FILES:
+                    validation_status=False
+                    with open(self.config.STATUS_FILE,'w') as status:
+                        status.write(f"Validation Status: {validation_status} for the file {file}.")
+                    logger.info(f"Data validation unsuccessful.")
+                else:
+                    validation_status=True
+                    with open(self.config.STATUS_FILE,'w') as status:
+                        status.write(f"Validation Status: {validation_status} for the all files.")
+                    logger.info("Data Validation Succesful.")
+            return validation_status
+        except Exception as e:
+            raise e                
