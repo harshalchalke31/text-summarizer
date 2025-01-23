ad1ca0c03ae139eb8fdd22c82a025c85713aceb8
harshalchalke31 <harshal.chalke2001@gmail.com>
2025-01-23 10:57:50 -0500

    model trainer test complete; entity, configuration manager, component updated

---
+++ src/textSummarizer/components/model_trainer.py
@@ -0,0 +1,52 @@
+import os
+from dataclasses import dataclass
+from pathlib import Path
+from textSummarizer.constants import *
+from textSummarizer.utils.common import read_yaml,create_directories
+from textSummarizer.entity import ModelTrainerConfig
+
+from transformers import Trainer, TrainingArguments, DataCollatorForSeq2Seq, AutoModelForSeq2SeqLM, AutoTokenizer
+from datasets import load_dataset, load_from_disk
+import torch
+
+class ModelTrainer:
+    def __init__(self, config: ModelTrainerConfig):
+        self.config = config
+
+    def train(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
+        model = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
+        datacollator = DataCollatorForSeq2Seq(tokenizer=tokenizer,model=model)
+
+        # loading data
+        dataset_samsum_pt = load_from_disk(self.config.data_path)
+
+        trainer_args = TrainingArguments(
+            output_dir=self.config.root_dir,
+            num_train_epochs=self.config.num_train_epochs,
+            warmup_steps=self.config.warmup_steps,
+            per_device_train_batch_size=self.config.per_device_train_batch_size,
+            per_device_eval_batch_size=self.config.per_device_train_batch_size,
+            weight_decay=self.config.weight_decay, 
+            logging_steps=self.config.logging_steps,
+            evaluation_strategy=self.config.evaluation_strategy, 
+            eval_steps=self.config.eval_steps, 
+            save_steps=1e6,
+            gradient_accumulation_steps=self.config.gradient_accumulation_steps
+        )
+
+        trainer = Trainer(
+            model=model,
+            args=trainer_args,
+            tokenizer=tokenizer,
+            data_collator=datacollator,
+            train_dataset=dataset_samsum_pt['test'],
+            eval_dataset=dataset_samsum_pt['validation']
+        )
+
+        trainer.train()
+
+        model.save_pretrained(os.path.join(self.config.root_dir,"pegasus_samsum_model_01"))
+
+        tokenizer.save_pretrained(os.path.join(self.config.root_dir,"pegasus_samsum_tokenizer_01"))
---
+++ src/textSummarizer/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,4 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.entity import ModelTrainerConfig
+
+
