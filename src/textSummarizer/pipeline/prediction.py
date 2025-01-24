7291cc02fe9e7df3922195daa283886509fb5296
harshalchalke31 <harshal.chalke2001@gmail.com>
2025-01-23 23:06:30 -0500

    application working

--- src/textSummarizer/pipeline/prediction.py
+++ src/textSummarizer/pipeline/prediction.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer, pipeline
 
 class PredictionPipeline:
     def __init__(self):
-        self.config = ConfigurationManager.get_model_evaluation_config()
+        self.config = ConfigurationManager().get_model_evaluation_config()
 
     def predict(self,text):
         tokenizer = AutoTokenizer.from_pretrained(self.config.tokenizer_path)
