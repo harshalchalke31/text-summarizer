a154f4935352a76962f67f7837146d02fccdff79
harshalchalke31 <harshal.chalke2001@gmail.com>
2025-01-25 17:25:55 -0500

    model training pipeline working

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from pathlib import Path
+from typing import List
+
 # create entity
 @dataclass(frozen=True)
 class DataIngestionConfig:
@@ -20,6 +22,21 @@ class DataTransformationConfig:
     data_path: Path
     TOKENIZER: Path
 
+# @dataclass(frozen=True)
+# class ModelTrainerConfig:
+#   root_dir: Path
+#   data_path: Path
+#   model_ckpt: Path
+#   num_train_epochs: int
+#   warmup_steps: int
+#   per_device_train_batch_size: int
+#   weight_decay: float
+#   logging_steps: int
+#   evaluation_strategy: str
+#   eval_steps: int
+#   save_steps: float
+#   gradient_accumulation_steps: int 
+
 @dataclass(frozen=True)
 class ModelTrainerConfig:
   root_dir: Path
@@ -33,8 +50,14 @@ class ModelTrainerConfig:
   evaluation_strategy: str
   eval_steps: int
   save_steps: float
-  gradient_accumulation_steps: int 
-
+  gradient_accumulation_steps: int
+  #updated
+  learning_rate: float
+  save_total_limit: int
+  load_best_model_at_end: bool
+  metric_for_best_model: str
+  fp16: bool
+  report_to: List[str]
 
 @dataclass(frozen=True)
 class ModelEvaluationConfig:
